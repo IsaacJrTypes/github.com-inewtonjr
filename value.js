2fbbc327d28810c5c3328c73d4f027a0d7fb3ef1
Isaac J <duckhunterjr@gmail.com>
2021-03-30 16:15:47 -0700

    add value checker assignmnet

---
+++ value.js
@@ -0,0 +1,13 @@
+
+function valueChecker(val1, val2) {
+    if (val1 === val2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(valueChecker(2, 4));
+console.log(valueChecker(2, 2));
+console.log(valueChecker('code','code'));
