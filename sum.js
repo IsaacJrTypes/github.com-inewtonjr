ecc50fb836c1ebc1a7571795fdbe9130bca32d0e
Isaac J <duckhunterjr@gmail.com>
2021-04-02 13:18:47 -0700

    add sum calculator js file

---
+++ sum.js
@@ -0,0 +1,11 @@
+
+function sumCalculator(numArray) {
+    let sum = 0;
+    for (let index = 0; index < numArray.length; index++) {
+        sum = sum + numArray[index]
+    }
+    return sum
+}
+let result=sumCalculator([4,6,9])
+
+console.log(result)
