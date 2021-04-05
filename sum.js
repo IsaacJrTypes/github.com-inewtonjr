e63aca82177eb5d3ac31c4f0936d6fa2bb2b8685
Isaac J <duckhunterjr@gmail.com>
2021-04-05 12:26:11 -0700

    add rewrite of fuction

--- sum.js
+++ sum.js
@@ -1,11 +1,11 @@
 
-function sumCalculator(numArray) {
+let sumCalculator = (numArray) => {
     let sum = 0;
     for (let index = 0; index < numArray.length; index++) {
         sum = sum + numArray[index];
     }
     return sum;
 }
-let result=sumCalculator([4,6,9]);
+let result = sumCalculator([4, 6, 9]);
 
 console.log(result)
