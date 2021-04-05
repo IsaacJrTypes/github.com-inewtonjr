d8b5917081ab7b613e70f50a86f4661105549c85
Isaac J <duckhunterjr@gmail.com>
2021-04-05 12:09:52 -0700

    add rewrite with arrow function

--- value.js
+++ value.js
@@ -1,5 +1,5 @@
 
-function valueChecker(val1, val2) {
+let valueChecker = (val1, val2) => {
     if (val1 === val2) {
         return true;
     }
@@ -10,4 +10,4 @@ function valueChecker(val1, val2) {
 
 console.log(valueChecker(2, 4));
 console.log(valueChecker(2, 2));
-console.log(valueChecker('code','code'));
+console.log(valueChecker('code', 'code'));
